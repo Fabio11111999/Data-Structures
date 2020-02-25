b8246f1a160527ee0aff00c9b94b2a84e0e7ede2
Fabio Marchesi <fabio.marchesi.99@gmail.com>
2020-02-25 12:37:40 +0100

    Fixed struct fraction

--- Fraction/fraction.cpp
+++ Fraction/fraction.cpp
@@ -1,57 +1,62 @@
 #include<bits/stdc++.h>
 using namespace std;
 struct fraction{
-	long long num, den;
+	typedef long long T;
+	T num, den;
+	fraction(): num(0), den(1) {}
+	fraction(T n, T d): num(n), den(d) {} 
 	void reduce(){
-		long long gcd=__gcd(num,den);
-		num/=gcd;
-		den/=gcd;
+		T gcd=__gcd(num,den);
+		num/=gcd, den/=gcd;
 	}
 	bool operator<(const fraction &other)const{
-		long long mcm=(den*other.den)/__gcd(den,other.den);
-		return other.num*(mcm/other.den)>num*(mcm/den);
+		T lcm=(den*other.den)/__gcd(den,other.den);
+		return other.num*(lcm/other.den)>num*(lcm/den);
 	}
 	bool operator>(const fraction &other)const{
-		long long mcm=(den*other.den)/__gcd(den,other.den);
-		return other.num*(mcm/other.den)<num*(mcm/den);
+		T lcm=(den*other.den)/__gcd(den,other.den);
+		return other.num*(lcm/other.den)<num*(lcm/den);
 	}
 	bool operator==(const fraction &other)const{
-		long long mcm=(den*other.den)/__gcd(den,other.den);
-		return num*(mcm/den)==other.num*(mcm/other.den);
+		return (!(*this<other) && !(*this>other));
 	}
 	fraction operator+(const fraction &other)const{
-		long long mcm=(den*other.den)/__gcd(den,other.den);
-		fraction res={num*(mcm/den)+other.num*(mcm/other.den),mcm};
+		T lcm=(den*other.den)/__gcd(den,other.den);
+		fraction res={num*(lcm/den)+other.num*(lcm/other.den),lcm};
 		res.reduce();
 		return res;
 	}
 	fraction operator-(const fraction &other)const{
-		long long mcm=(den*other.den)/__gcd(den,other.den);
-		fraction res={num*(mcm/den)-other.num*(mcm/other.den),mcm};
+		T lcm=(den*other.den)/__gcd(den,other.den);
+		fraction res={num*(lcm/den)-other.num*(lcm/other.den),lcm};
 		res.reduce();
 		return res;
 	}
 	fraction operator*(const fraction &other)const{
 		fraction a=*this, b=other;
 		a.reduce(); b.reduce();
-		long long g1=__gcd(a.num,b.den), g2=__gcd(a.den,b.num);
+		T g1=__gcd(a.num,b.den), g2=__gcd(a.den,b.num);
 		a.num/=g1; a.den/=g2; b.num/=g2; b.den/=g1;
 		return {a.num*b.num,a.den*b.den};
 	}
 	fraction operator/(const fraction &other)const{
 		return (*this)*(fraction){other.den,other.num};
 	}
-	void operator+=(const fraction &other){
+	fraction& operator+=(const fraction &other){
 		*this=*this+other;
+		return *this;
 	}
-	void operator-=(const fraction &other){
+	fraction& operator-=(const fraction &other){
 		*this=*this-other;
+		return *this;
 	}
-	void operator*=(const fraction &other){
+	fraction& operator*=(const fraction &other){
 		*this=*this*other;
+		return *this;
 	}
-	void operator/=(const fraction &other){
+	fraction& operator/=(const fraction &other){
 		*this=*this/other;
+		return *this;
 	}
 	
 };
@@ -59,19 +64,10 @@ ostream& operator<<(ostream& os, fraction& f){
 	os<<'['<<f.num<<','<<f.den<<"]\n";
 	return os;
 }
+//Make sure that the type of a and b is compatible with T
 istream& operator>>(istream& is, fraction& f){
 	long long a, b;
 	cin>>a>>b;
 	f=(b==0)?(fraction){a,1}:(fraction){a,b};
 	return is;
-}
-int main(){
-	int n;
-	cin>>n;
-	vector<fraction> v(n);
-	for(int i=0;i<n;i++)
-		cin>>v[i];
-	sort(v.begin(),v.end());
-	for(int i=0;i<n;i++)
-		cout<<v[i];
 }
