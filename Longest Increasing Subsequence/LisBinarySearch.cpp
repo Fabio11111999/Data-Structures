3ebae6bd7fa6e16d4c7aa609ed9397e90fbe75d4
Fabio Marchesi <39414882+Fabio11111999@users.noreply.github.com>
2020-05-05 17:56:49 +0200

    Update LisBinarySearch.cpp

--- Longest Increasing Subsequence/LisBinarySearch.cpp
+++ Longest Increasing Subsequence/LisBinarySearch.cpp
@@ -1,35 +1,13 @@
-//longest increasing subsequence O(NlogN)
 #include<bits/stdc++.h>
 using namespace std;
-ifstream in("input.txt");
-ofstream out("output.txt");
-int const MAXN=10010;
-int n, lis[MAXN], last=0;
-int main(){
-	in>>n;
-	for(int i=0;i<n;i++){
-		int val;
-		in>>val;
-		if(i==0)
-			lis[0]=val;
-		else{
-			if(lis[0]>val)
-				lis[0]=val;
-			else if(lis[last]<val)
-				lis[++last]=val;
-			else{	
-				int s=0, m, e=last, sol;
-				while(s<=e){
-					m=(s+e)/2;
-					if(lis[m]>=val){
-						sol=m;
-						e=m-1;
-					}else
-						s=m+1;
-				}
-				lis[sol]=min(lis[sol],val);
-			}
+template<class T> class LIS{
+	public:
+		void add(T x){
+			if(lowest.empty())	lowest.push_back(x);
+			else if(lowest.back()<x) lowest.push_back(x);
+			else *(lower_bound(lowest.begin(),lowest.end(),x))=x;
 		}
-	}
-	out<<last+1;
-}
+		int size(){ return (int) lowest.size(); } 
+	private:
+		vector<T> lowest;
+};
