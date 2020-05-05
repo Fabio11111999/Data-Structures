e97ca0cb481c16c012c4432fbe091e5af9cac978
Fabio Marchesi <39414882+Fabio11111999@users.noreply.github.com>
2020-05-05 17:58:11 +0200

    Update LisBinarySearch.cpp

--- Longest Increasing Subsequence/LisBinarySearch.cpp
+++ Longest Increasing Subsequence/LisBinarySearch.cpp
@@ -1,10 +1,10 @@
+#pragma GCC optimize("O3")
 #include<bits/stdc++.h>
 using namespace std;
 template<class T> class LIS{
 	public:
 		void add(T x){
-			if(lowest.empty())	lowest.push_back(x);
-			else if(lowest.back()<x) lowest.push_back(x);
+			if(lowest.empty() || lowest.back()<x)	lowest.push_back(x);
 			else *(lower_bound(lowest.begin(),lowest.end(),x))=x;
 		}
 		int size(){ return (int) lowest.size(); } 
