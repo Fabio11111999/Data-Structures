11d79130dfaad3c59d229b51b41db66c4cfb2c8b
Fabio Marchesi <39414882+Fabio11111999@users.noreply.github.com>
2020-08-19 14:39:01 +0200

    Create karp_rabin.cpp

---
+++ karp_rabin.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int count_karp_rabin(string text, string pattern, int alph_size, int prime, int offset='a'){
+	if(text.length()<pattern.length())
+		return 0;
+	long long diff=1LL, current=0LL, hash=0LL;
+	for(int i=0;i<pattern.length()-1;i++)
+		diff=(diff*alph_size)%prime;
+	for(int i=0;i<pattern.length();i++){
+		current=(current*alph_size+text[i]-offset)%prime;
+		hash=(hash*alph_size+pattern[i]-offset)%prime;
+	}
+	int ans=(current==hash);
+	for(int i=pattern.length();i<text.length();i++){
+		current=((current-((text[i]-offset)*diff))*alph_size+text[i]-offset)%prime;
+		while(current<0) current+=prime;
+		if(current==hash) 
+			ans++;
+	}
+	return ans;
+}
