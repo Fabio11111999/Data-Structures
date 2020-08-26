0a77a845845e652979a68c7d9eb89d2d877d2a90
Fabio Marchesi <39414882+Fabio11111999@users.noreply.github.com>
2020-08-26 10:59:00 +0200

    Update karp_rabin.cpp

--- karp_rabin.cpp
+++ karp_rabin.cpp
@@ -4,15 +4,15 @@ int count_karp_rabin(string text, string pattern, int alph_size, int prime, int 
 	if(text.length()<pattern.length())
 		return 0;
 	long long diff=1LL, current=0LL, hash=0LL;
-	for(int i=0;i<pattern.length()-1;i++)
+	for(int i=0;i<(int)pattern.length()-1;i++)
 		diff=(diff*alph_size)%prime;
-	for(int i=0;i<pattern.length();i++){
+	for(int i=0;i<(int)pattern.length();i++){
 		current=(current*alph_size+text[i]-offset)%prime;
 		hash=(hash*alph_size+pattern[i]-offset)%prime;
 	}
 	int ans=(current==hash);
-	for(int i=pattern.length();i<text.length();i++){
-		current=((current-((text[i]-offset)*diff))*alph_size+text[i]-offset)%prime;
+	for(int i=(int)pattern.length();i<(int)text.length();i++){
+		current=((current-((text[i-(int)pattern.length()]-offset)*diff))*alph_size+text[i]-offset)%prime;
 		while(current<0) current+=prime;
 		if(current==hash) 
 			ans++;
