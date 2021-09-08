3988109e683ac24fc9082f979ff675db3f187388
Fabio Marchesi <39414882+Fabio11111999@users.noreply.github.com>
2021-09-08 10:13:53 +0200

    Update RangeQueryPointUpdate2.cpp

--- SegmentTree/RangeQueryPointUpdate2.cpp
+++ SegmentTree/RangeQueryPointUpdate2.cpp
@@ -2,11 +2,12 @@ template<class T, T merge(T, T)> class SegmentTree {
 	
 	unsigned int sz;
 	vector<T> segtree;
-	vector<T> leaves;
-
+	vector<int> pos_leaves;
+ 
 	void build_default(unsigned int l, unsigned int r, unsigned int pos, T def) {
 		if (l == r) {
 			segtree[pos] = def;
+			pos_leaves[l] = pos;
 		} else {
 			unsigned int mid = (l + r) / 2;
 			build_default(l, mid, pos * 2 + 1, def);
@@ -14,10 +15,11 @@ template<class T, T merge(T, T)> class SegmentTree {
 			segtree[pos] = merge(segtree[pos * 2 + 1], segtree[pos * 2 + 2]);
 		}
 	}
-
+ 
 	void build(unsigned int l, int unsigned r, unsigned int pos, vector<T> &v) {
 		if (l == r) {
 			segtree[pos] = v[l];
+			pos_leaves[l] = pos;
 		} else {
 			unsigned int mid = (l + r) / 2;
 			build(l, mid, pos * 2 + 1, v);
@@ -25,22 +27,7 @@ template<class T, T merge(T, T)> class SegmentTree {
 			segtree[pos] = merge(segtree[pos * 2 + 1], segtree[pos * 2 + 2]);
 		}
 	}
-	
-	void update(unsigned int l, unsigned int r, unsigned int pos, unsigned int i, const T &val) {
-		if (l == r) {
-			segtree[pos] = val;
-			leaves[l] = val;
-		} else {
-			unsigned int mid = (l + r) / 2;
-			if (i <= mid) {
-				update(l, mid, pos * 2 + 1, i, val);
-			} else {
-				update(mid + 1, r, pos * 2 + 2, i, val);
-			}
-			segtree[pos] = merge(segtree[pos * 2 + 1], segtree[pos * 2 + 2]);
-		}
-	}
-
+	 
 	T query(unsigned int l, unsigned int r, unsigned int pos, unsigned int start, unsigned int end) const {
 		if (start <= l && end >= r) {
 			return segtree[pos];
@@ -55,7 +42,7 @@ template<class T, T merge(T, T)> class SegmentTree {
 			}
 		}	
 	}
-
+ 
 	// std::ostream &operator must be defined for type T
 	void print(unsigned int l, unsigned int r, unsigned int pos) const {
 		if (l != r) {
@@ -66,76 +53,94 @@ template<class T, T merge(T, T)> class SegmentTree {
 		cout << "Segment = [" + to_string(l) + ", " + to_string(r) + "] ";
 		cout << "Value = " << segtree[pos] << endl;
 	}
-
+ 
 public:
-
+ 
 	SegmentTree(unsigned int n, const T &def) : sz(n) {
 		assert(n > 0);
 		segtree.resize(4 * sz);
-		leaves.resize(sz);
-		fill(leaves.begin(), leaves.end(), def);
+		pos_leaves.resize(sz);
 		build_default(0, sz - 1, 0, def);
 	}
-
-	SegmentTree(vector<T> &v) : sz(v.size()), leaves(v) {
+ 
+	SegmentTree(vector<T> &v) : sz(v.size()) {
 		assert(v.size() > 0);
 		segtree.resize(4 * sz);
+		pos_leaves.resize(sz);
 		build(0, sz - 1, 0, v);
 	}
-
+ 
 	template<typename IterT> SegmentTree(IterT begin, IterT end) {
-		leaves = vector<T>(begin, end);
-		sz = leaves.size();
+		vector<T> elements(begin, end);
+		sz = elements.size();
 		assert(sz > 0);
 		segtree.resize(4 * sz);
-		build(0, sz - 1, 0, leaves);
+		pos_leaves.resize(sz);
+		build(0, sz - 1, 0, elements);
 	}
 	
 	SegmentTree(const SegmentTree &other) : sz(other.sz) {
 		segtree = other.segtree;
-		leaves = other.leaves;
+		pos_leaves = other.pos_leaves;
+		sz = other.sz;
 	}
-
+ 
 	SegmentTree& operator=(const SegmentTree &other) {
 		if (this != &other) {
 			SegmentTree tmp(other);
 			swap(sz, tmp.sz);
 			swap(segtree, tmp.segtree);
-			swap(leaves, tmp.leaves);
+			swap(pos_leaves, tmp.pos_leaves);
 		}
 		return *this;
 	}
-
+ 
 	bool operator==(const SegmentTree &other) const {
-		return leaves == other.leaves;
+		if (sz != other.sz) {
+			return false;
+		}
+		for (int pos : pos_leaves) {
+			if (segtree[pos] != other.segtree[pos]) {
+				return false;
+			}
+		}
+		return true;
 	}	
-
+ 
 	~SegmentTree() {}
-
+ 
 	void update(unsigned int i, const T &val) {
 		assert(i < sz);
-		update(0, sz - 1, 0, i, val);
+		segtree[pos_leaves[i]] = val;
+		if (pos_leaves[i] > 0) {
+			int pos = (pos_leaves[i] - 1) / 2;
+			while (pos != 0) {
+				segtree[pos] = merge(segtree[pos * 2 + 1], segtree[pos * 2 + 2]);
+				pos = (pos - 1) / 2;
+			}	
+			segtree[0] = merge(segtree[1], segtree[2]);
+		}
 	}
-
+ 
 	T query(unsigned int start, unsigned int end) const {
 		assert(start <= end);
 		assert(end < sz);
 		return query(0, sz - 1, 0, start, end);
 	}
-
+ 
 	unsigned int size() const {
 		return sz;
 	}
 	
 	T operator[](unsigned int index) const {
 		assert(index < sz);
-		return leaves[index];
+		return segtree[pos_leaves[index]];
 	}
-
+ 
 	void print() const {
 		cout << "Leaves = [";
-		for (unsigned int i = 0; i < leaves.size(); i++) {
-			cout << leaves[i] << ",]"[i == leaves.size() - 1];
+		for (unsigned int i = 0; i < pos_leaves.size(); i++) {
+			cout << segtree[pos_leaves[i]] << ",]"[i == sz - 1];
 		}
 		cout << endl;
 		print(0, sz - 1, 0);
